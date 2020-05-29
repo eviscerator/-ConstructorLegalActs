ed17783d050900e3cb3351abb1eb8ee455ec1032
ivkodg <ivkodg@altarix.ru>
2020-05-29 10:46:08 +0300

    init

---
+++ build/precache-manifest.7602e4535b5c929d78efd397493a13b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95ef51d31f3d9fabca79d10fe9ef56ea",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "f6636c817e894f3c98a1",
+    "url": "/build/static/css/2.bea6f0b1.chunk.css"
+  },
+  {
+    "revision": "563674f7358aacfca90d",
+    "url": "/build/static/css/main.c308bcde.chunk.css"
+  },
+  {
+    "revision": "f6636c817e894f3c98a1",
+    "url": "/build/static/js/2.3e813523.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/build/static/js/2.3e813523.chunk.js.LICENSE"
+  },
+  {
+    "revision": "563674f7358aacfca90d",
+    "url": "/build/static/js/main.478b0998.chunk.js"
+  },
+  {
+    "revision": "3661ef522d03fd983562",
+    "url": "/build/static/js/runtime-main.f653bfb6.js"
+  }
+]);

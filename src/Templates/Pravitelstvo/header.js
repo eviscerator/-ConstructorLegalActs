c41efe57e8e1e74c8d8af1fad6af809c31f2783d
ivkodg <ivkodg@altarix.ru>
2020-05-31 18:17:41 +0300

    fix style

--- src/Templates/Pravitelstvo/header.js
+++ src/Templates/Pravitelstvo/header.js
@@ -1,4 +1,13 @@
 import React from 'react'
+import { DatePicker, AutoComplete, Input } from 'antd'
+
+const options = [
+  { value: 'Burns Bay Road' },
+  { value: 'Downing Street' },
+  { value: 'Wall Street' },
+]
+
+const handleSearch = (inputValue, option) => option.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
 
 export default () => (
   <>
@@ -9,13 +18,13 @@ export default () => (
       ПОСТАНОВЛЕНИЕ
     </h3>
     <p style={{ textAlign: 'center', fontFamily: 'Times New Roman', fontSize: 19 }}>
-      [<span style={{ color: 'blue' }}>дата</span>] № [<span style={{ color: 'blue' }}>номер</span>]
+      <span style={{ color: 'blue', position: 'relative', cursor: 'pointer' }}><DatePicker format='DD.MM.YYYY' style={{ width: 100 }} placeholder='Дата' size='small' /></span> № <span style={{ color: 'blue' }}><Input placeholder='Номер' size='small' style={{ width: 150 }} /></span>
     </p>
     <p style={{ textAlign: 'center', fontFamily: 'Times New Roman', fontSize: 19 }}>
       Москва
     </p>
     <p style={{ textAlign: 'center', fontFamily: 'Times New Roman', fontSize: 19 }}>
-      «О внесении изменений в [<span style={{ color: 'blue' }}>название, дата, номер НПА</span>]»
+      «О внесении изменений в <span><AutoComplete placeholder='название' size='small' style={{ width: 200 }} options={options} onChange={handleSearch} />, <DatePicker format='DD.MM.YYYY' style={{ width: 100 }} placeholder='Дата' size='small' />, <AutoComplete placeholder='номер НПА' size='small' style={{ width: 150 }} options={options} onChange={handleSearch} /></span>»
     </p>
     <div style={{ margin: 20 }}>
       <p style={{ textAlign: 'start', fontFamily: 'Times New Roman', margin: 0, fontSize: 19 }}>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-
+import { ConfigProvider } from 'antd'
+import ruRu from 'antd/es/locale/ru_RU'
+import moment from 'moment'
+import 'moment/locale/ru'
 import 'antd/dist/antd.css'
 import './index.scss'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+moment.locale('ru')
+
+ReactDOM.render(<ConfigProvider locale={ruRu}><App /></ConfigProvider>, document.getElementById('root'))
